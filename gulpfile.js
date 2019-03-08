ec0872bc39b8314381dfb3186e4526c105fa4779
Jason <16302010059@gmail.com>
2019-03-08 14:48:40 +0800

    使用gulp自动化导入html、编译Scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
 var fileinclude = require('gulp-file-include'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
     gulp = require('gulp');
 
-gulp.task('fileinclude', function () {
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('compressCss', function () {
+    gulp.src('src/css/*.scss').pipe(sass())
+        .pipe(minifyCss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('fileInclude', function () {
     gulp.src(['src/html/**/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
@@ -10,7 +30,7 @@ gulp.task('fileinclude', function () {
         .pipe(gulp.dest('dist/html/'));
 });
 
-gulp.task('indexinclude', function () {
+gulp.task('indexInclude', function () {
     gulp.src(['src/html/index.html'])
         .pipe(fileinclude({
             prefix: '@@',
@@ -18,3 +38,6 @@ gulp.task('indexinclude', function () {
         }))
         .pipe(gulp.dest('./'));
 });
+
+
+gulp.task('default', ['indexInclude', 'fileInclude', 'compressCss']);
